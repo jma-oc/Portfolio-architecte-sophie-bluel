3c86ede29fe7eaa1aaf8a0f2f57e745ab1da534e
Julien MATHIEU <julien.mathieu@example.com>
2026-01-27 11:06:12 +0100

    Gestion d'erreurs d'authentification

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,31 +1,54 @@
-const loginForm = document.getElementById("login-form");
+// Redirige l'utilisateur vers la page principale s'il est déjà connecté
+const loginPageCheck = () => {
+  const isLoggedIn = !!localStorage.getItem("authToken"); // Valeur booléenne
+  if (!isLoggedIn) return; // Si aucun token, on sort de la fonction
+  window.location.href = "index.html";
+};
+loginPageCheck();
 
-loginForm.addEventListener("submit", async (event) => {
+const submitListener = () => {
+  console.log("Script chargé !");
+  const loginForm = document.getElementById("login-form");
+  const errorMessage = document.getElementById("error-message");
+
+  if (!loginForm) return;
+
+  loginForm.addEventListener("submit", async (event) => {
     event.preventDefault();
 
-    const formData = new FormData(loginForm); // Récupère les données du formulaire
-    const payload = Object.fromEntries(formData); // Convertit en objet JS
+    // Reset du message d'erreur à chaque tentative
+    if (errorMessage) {
+      errorMessage.textContent = "";
+      errorMessage.classList.remove("login-form__error--border");
+    }
+
+    const formData = new FormData(loginForm);
+    const payload = Object.fromEntries(formData);
 
     try {
-        const response = await fetch("http://localhost:5678/api/users/login", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(payload), // Envoie les données au format JSON
-        });
+      const response = await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload),
+      });
 
-        if (!response.ok) {
-            throw new Error(response.status === 401 || response.status === 404 // 401 Unauthorized ou 404 Not Found
-                ? "Identifiants incorrects" 
-                : "Erreur serveur");
-        }
+      if (!response.ok) {
+        throw new Error("Erreur d'authentification.");
+      }
 
-        const { token } = await response.json();
-        
-        localStorage.setItem("authToken", token);
-        window.location.href = "index.html";
+      const data = await response.json();
+      
+      localStorage.setItem("authToken", data.token);
+      
+      window.location.href = "index.html";
 
     } catch (error) {
-        console.error("Échec de la connexion :", error.message);
-        alert(error.message || "Une erreur est survenue.");
+      console.error("Échec de la connexion :", error.message);
+      if (errorMessage) {
+        errorMessage.textContent = "Erreur d'authentification.";
+        errorMessage.classList.add("login-form__error--border");
+      }
     }
-});
+  });
+};
+submitListener();
